742d7a4565f2b9086b6a1705ce7bb818d1f01fa9
NET_12_PhanLongHoangPhuc <phucplhse173164@fpt.edu.vn>
2024-10-07 22:10:49 +0700

    fisrt init

---
+++ main.js
@@ -0,0 +1,29 @@
+var fullName = 'Phan Long Hoang Phuc';
+
+var age = 21;
+var a = typeof(age);
+console.log(typeof(a))
+function ShowLog(){
+    var n = 100/ 100;
+    if (n){
+        return true;
+    }
+    else return false;
+}
+
+
+
+// Thuc thi bien
+// alert(fullName);
+// alert(age);
+
+
+/* 
+Giới thiệu một số hàm built-in 
+1. Alert : Boxup hiện tin nhắn
+2. Console : Có chứ nhiều hàm bên trong, hiển thị ở cửa sổ console của trình duyệt
+3. Confirm : hộp thoại yes no
+4. Prompt : 
+5. Set timeout
+6. Set interval
+*/
